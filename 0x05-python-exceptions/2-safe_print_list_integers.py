74b83d1aa7f182541cc783981a5a622c6949dd7c
root <christinechep970@gmail.com>
2022-06-20 14:30:17 -0700

    divides 2 integers and prints the result.

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,15 +1,9 @@
 #!/usr/bin/python3
 
-
-
-
-
 def safe_print_list_integers(my_list=[], x=0):
 
     elements = 0
-
     for i in range(x):
-
         try:
 
             print("{:d}".format(my_list[i]), end="")
@@ -21,5 +15,4 @@ def safe_print_list_integers(my_list=[], x=0):
             continue
 
     print()
-
     return 

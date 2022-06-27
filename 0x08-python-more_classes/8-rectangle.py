3e19132d58387e7268b7b1ba86d181b78174c61b
root <christinechep970@gmail.com>
2022-06-27 13:01:52 -0700

    class Rectangle that defines a rectangle by: (based on 8-rectangle.py

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -7,6 +7,8 @@ class Rectangle:
 
     number_of_instances = 0
 
+    print_symbol = "#"
+
     def __init__(self, width=0, height=0):
 
         """Instantiates the class"""
@@ -62,6 +64,7 @@ class Rectangle:
         if value < 0:
 
             raise ValueError("height must be >= 0")
+
         self.__height = value
 
     def area(self):
@@ -82,6 +85,32 @@ class Rectangle:
 
             return (self.__width + self.__height) * 2
 
+    @staticmethod
+
+    def bigger_or_equal(rect_1, rect_2):
+
+        """Return the Rectangle with the greater area.
+
+        Raises:
+
+            TypeError: If either of rect_1 or rect_2 is not a Rectangle.
+
+        """
+
+        if not isinstance(rect_1, Rectangle):
+
+            raise TypeError("rect_1 must be an instance of Rectangle")
+
+        if not isinstance(rect_2, Rectangle):
+
+            raise TypeError("rect_2 must be an instance of Rectangle")
+
+        if rect_1.area() >= rect_2.area():
+
+            return (rect_1)
+
+        return (rect_2)
+
     def __str__(self):
 
         """Prints the rectangle"""
@@ -96,7 +125,7 @@ class Rectangle:
 
             for j in range(self.__width):
 
-                new.append('#')
+                new.append(str(self.print_symbol))
 
             if i != self.__height - 1:
 
--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -1,11 +1,23 @@
 #!/usr/bin/python3
-# 5-rectangle.py
+# 9-rectangle.py
 
 """Defines a Rectangle class."""
 
 class Rectangle:
 
-    """Represent a rectangle."""
+    """Represent a rectangle.
+
+    Attributes:
+
+        number_of_instances (int): The number of Rectangle instances.
+
+        print_symbol (any): The symbol used for string representation.
+
+    """
+
+    number_of_instances = 0
+
+    print_symbol = "#"
 
     def __init__(self, width=0, height=0):
 
@@ -19,6 +31,8 @@ class Rectangle:
 
         """
 
+        type(self).number_of_instances += 1
+
         self.width = width
 
         self.height = height
@@ -30,7 +44,6 @@ class Rectangle:
         """Get/set the width of the Rectangle."""
 
         return self.__width
-
 
     @width.setter
 
@@ -84,6 +97,52 @@ class Rectangle:
 
         return ((self.__width * 2) + (self.__height * 2))
 
+    @staticmethod
+
+    def bigger_or_equal(rect_1, rect_2):
+
+        """Return the Rectangle with the greater area.
+
+        Args:
+
+            rect_1 (Rectangle): The first Rectangle.
+
+            rect_2 (Rectangle): The second Rectangle.
+
+        Raises:
+
+            TypeError: If either of rect_1 or rect_2 is not a Rectangle.
+
+        """
+
+        if not isinstance(rect_1, Rectangle):
+
+            raise TypeError("rect_1 must be an instance of Rectangle")
+
+        if not isinstance(rect_2, Rectangle):
+
+            raise TypeError("rect_2 must be an instance of Rectangle")
+
+        if rect_1.area() >= rect_2.area():
+
+            return (rect_1)
+
+        return (rect_2)
+
+    @classmethod
+
+    def square(cls, size=0):
+
+        """Return a new Rectangle with width and height equal to size.
+
+        Args:
+
+            size (int): The width and height of the new Rectangle.
+
+        """
+
+        return (cls(size, size))
+
     def __str__(self):
 
         """Return the printable representation of the Rectangle.
@@ -100,7 +159,7 @@ class Rectangle:
 
         for i in range(self.__height):
 
-            [rect.append('#') for j in range(self.__width)]
+            [rect.append(str(self.print_symbol)) for j in range(self.__width)]
 
             if i != self.__height - 1:
 
@@ -121,5 +180,7 @@ class Rectangle:
     def __del__(self):
 
         """Print a message for every deletion of a Rectangle."""
+
+        type(self).number_of_instances -= 1
 
         print("Bye rectangle...")
